530026daaedd27f5079de6ac4ed8d6d0a6bab706
Siva_Kumar_S1 <SivaKumar.S@dell.com>
2023-05-04 16:21:27 +0530

    Added Multiply.ps1 & Fresh Readme.md file

---
+++ Multiply.ps1
@@ -0,0 +1,9 @@
+## Powershell Code for Multiplication of two numbers
+
+[int]$var1 = Read-Host "Enter Number1 for Var1"
+[int]$var2 = Read-Host "Enter Number2 for Var2"
+
+[int]$var3 = $var1*$var2
+
+Write-Output $var3
+
